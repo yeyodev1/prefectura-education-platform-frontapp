1375817768e3668444b4aff1ee64deb6302b5ed4
Diego Reyes <diegorele13@gmail.com>
2025-12-09 04:04:13 -0500

    feat(users): add login method to users service

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -2,10 +2,23 @@ import type { AxiosRequestConfig, AxiosResponse } from 'axios'
 import APIBase from './httpBase'
 
 export interface CreateUserBody extends Record<string, unknown> { }
+export interface LoginBody {
+  email: string
+  password: string
+}
+export interface LoginResponse {
+  message: string
+  token: string
+  user: Record<string, unknown>
+}
 
 class UsersService extends APIBase {
   async create<T>(body: CreateUserBody, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
     return this.post<T>('users', body, undefined, config)
+  }
+
+  async login<T = LoginResponse>(body: LoginBody, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.post<T>('users/login', body, { 'Content-Type': 'application/json' }, config)
   }
 }
 
