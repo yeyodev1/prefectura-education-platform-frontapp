7e3619911fd9df7a143fb5194d590bde402dacf6
Luis Reyes @lualreye <lualreye@gmail.com>
2025-12-09 02:26:17 -0500

    FIX: layout

--- src/layout/UserLayout.vue
+++ src/layout/UserLayout.vue
@@ -4,34 +4,41 @@
 </script>
 
 <template>
-  <div class="header">
-    <UserHeader />
-  </div>
-  <div class="layout">
-    <div class="layout-menu-wrapper">
-      <UserSidebar />
+  <div class="wrapper">
+    <div class="header">
+      <UserHeader />
     </div>
-    <div class="layout-view-wrapper">
-      <RouterView />
+    <div class="layout">
+      <div class="layout-menu-wrapper">
+        <UserSidebar />
+      </div>
+      <div class="layout-view-wrapper">
+        <RouterView />
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.header {
-  width: 100%;
-}
-.layout {
-  display: flex;
-  height: 100vh;
-  &-menu-wrapper {
-    width: 100%;
-    max-width: fit-content;
-    min-width: 24px;
+  .wrapper {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    .header {
+      width: 100%;
+    }
+    .layout {
+      flex: 1;
+      display: flex;
+      &-menu-wrapper {
+        width: 100%;
+        max-width: fit-content;
+        min-width: 24px;
+      }
+      &-view-wrapper {
+        width: 100%;
+        flex: 1;
+      }
+    }
   }
-  &-view-wrapper {
-    width: 100%;
-    flex: 1;
-  }
-}
 </style>
